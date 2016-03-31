abbdf221118e06f55513a34d1238aedabccae6a5
LexLoki <pietroribeiropepe@hotmail.com>
2016-03-31 04:48:58 -0300

    Initial commit

---
+++ UIAnimation/UIAnimation.swift
@@ -0,0 +1,246 @@
+//
+//  UIAnimation.swift
+//  Created by Pietro Ribeiro Pepe on 3/31/16.
+
+//  The MIT License (MIT)
+//  Copyright © 2016 Pietro Ribeiro Pepe.
+
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import UIKit
+
+extension UIView{
+    /**
+     * Executes an 'UIAnimation' on the view
+     *
+     * - Parameter animation: A 'UIAnimation'
+    */
+    func runAnimation(animation : UIAnimation, completion : ()->Void = {}){
+        animation.execute(self,completion)
+    }
+    /**
+     * Removes all the 'UIAnimation' that are running on the view
+     */
+    func removeAllAnimations(){
+        layer.removeAllAnimations()
+    }
+    /// Not implemented
+    func removeAnimationForKey(key : String){
+        
+    }
+}
+/**
+ * Specifies an animation cycle that can be executed by an UIView
+ *
+ * - Movement
+ * - Rotation
+ * - Actions sequentially
+ * - Actions concurrently
+ * - Repeat
+ * - RunBlock
+ * - WaitTime
+*/
+class UIAnimation{
+    
+    /**
+     * Creates an action that moves an UIView's center point to a given point
+     *
+     * - Parameter point: A 'CGPoint' value specifying where the 'UIView' should move
+     * - Parameter duration: A 'NSTimerInverval' specifying the duration of the movement
+    */
+    class func moveTo(point : CGPoint, duration : NSTimeInterval) -> UIAnimation{
+        return UIAnimation(data: NSDictionary(objects: [NSValue(CGPoint: point),false], forKeys: ["point","isBy"]), duration, _handleMovement)
+    }
+    
+    /**
+     * Creates an action that moves an UIView's by a given distance
+     *
+     * - Parameter point: A 'CGPoint' value specifying how much the 'UIView' should move
+     * - Parameter duration: A 'NSTimerInverval' specifying the duration of the movement
+     */
+    class func moveBy(point : CGPoint, duration : NSTimeInterval) -> UIAnimation{
+        return UIAnimation(data: NSDictionary(objects: [NSValue(CGPoint: point),true], forKeys: ["point","isBy"]), duration, _handleMovement)
+    }
+    
+    /**
+     * Creates an action that rotates the UIView's to a given angle
+     *
+     * - Parameter angle: A 'CGFloat' value specifying the angle the 'UIView' should rotate to, in degrees
+     * - Parameter duration: A 'NSTimerInverval' specifying the duration of the rotation
+     */
+    class func rotateTo(angle : CGFloat, duration : NSTimeInterval) -> UIAnimation{
+        return UIAnimation(data: NSDictionary(objects: [angle,false], forKeys: ["angle","isBy"]), duration, _handleRotation)
+    }
+    
+    /**
+     * Creates an action that rotates clockwise the UIView's by a relative value.
+     *
+     * - Parameter angle: A 'CGFloat' value specifying the ammount the UIView should rotate, in degrees
+     * - Parameter duration: A 'NSTimerInverval' specifying the duration of the rotation
+     */
+    class func rotateBy(angle : CGFloat, duration : NSTimeInterval) -> UIAnimation{
+        return UIAnimation(data: NSDictionary(objects: [angle,true], forKeys: ["angle","isBy"]), duration, _handleRotation)
+    }
+    
+    /**
+     * Creates an action that runs a collection of actions sequentially
+     *
+     * - Parameter animations: An array of 'UIAnimation'
+     */
+    class func sequence(animations : [UIAnimation]) -> UIAnimation{
+        var c : NSTimeInterval = 0
+        for a in animations{
+            c += a.time
+        }
+        return UIAnimation(data: NSDictionary(object: animations, forKey: "animations"), c, _handleSequence)
+    }
+    
+    /**
+     * Creates an action that runs a collection of actions concurrently
+     *
+     * - Parameter animations: An array of 'UIAnimation'
+     */
+    class func group(animations : [UIAnimation]) -> UIAnimation{
+        var c : NSTimeInterval = 0
+        for a in animations{
+            if a.time>c{
+                c = a.time
+            }
+        }
+        return UIAnimation(data: NSDictionary(object: animations, forKey: "animations"), c, _handleGroup)
+    }
+    
+    /**
+     * Creates an action that repeats another action a specified number of times
+     *
+     * - Parameter animation: The action to repeat
+     * - Parameter count: how many times it should repeat
+     */
+    class func repeatAnimation(animation : UIAnimation, count : Int) -> UIAnimation{
+        return UIAnimation(data: NSDictionary(objects: [animation,count], forKeys: ["animation","repTime"]), animation.time*NSTimeInterval(count), _handleRepeat)
+    }
+    
+    /**
+     * Creates an action that repeats another action forever
+     *
+     * - Parameter animation: The action to repeat
+     */
+    class func repeatAnimationForever(animation : UIAnimation) -> UIAnimation{
+        return repeatAnimation(animation, count: -1)
+    }
+    
+    /**
+     * Creates an action that executes a block
+     *
+     * - Parameter block: The '()->Void' block to be executed
+     */
+    class func runBlock(block : ()->Void) -> UIAnimation{
+        return UIAnimation(data: NSDictionary(object: BlockWrapper(block), forKey: "block"), 0, _handleBlock)
+    }
+    
+    /**
+     * Creates an action that idles for a specified period of time
+     *
+     * - Parameter duration: A 'NSTimeInterval' with how many seconds it should idle
+     */
+    class func waitForDuration(duration : NSTimeInterval) -> UIAnimation{
+        return UIAnimation(data: NSDictionary(), duration, _handleWait)
+    }
+    
+    private class BlockWrapper{
+        let block: ()->Void
+        init(_ block: ()->Void){
+            self.block = block
+        }
+    }
+    
+    /// Duration of the animation
+    private let time : NSTimeInterval
+    /// Information of the animation
+    private let data : NSDictionary
+    /// The function to be called when a UIView runs the animation
+    private let handler : (UIView,UIAnimation,(()->Void)?)->Void
+    
+    private init(data : NSDictionary, _ time : NSTimeInterval, _ handler : (UIView,UIAnimation,(()->Void)?)->Void){
+        self.time = time
+        self.data = data
+        self.handler = handler
+    }
+    
+    func execute(view : UIView, _ completion : ()->Void){
+        handler(view,self,completion)
+    }
+    
+    class private func _handleMovement(view : UIView, _ anim : UIAnimation, _ comp: (()->Void)?){
+        var p = (anim.data["point"] as! NSValue).CGPointValue()
+        if (anim.data["isBy"] as! Bool){
+            p.x = view.center.x+p.x; p.y = view.center.y+p.y
+        }
+        UIView.animateWithDuration(anim.time, animations: { () -> Void in
+            view.center = p
+            }) { (v) -> Void in
+                if v{ comp?() }
+        }
+    }
+    
+    class private func _handleRotation(view : UIView, _ anim : UIAnimation, _ comp: (()->Void)?){
+        let a = (anim.data["angle"] as! CGFloat) / 180.0 * CGFloat(M_PI)
+        UIView.animateWithDuration(anim.time, animations: { () -> Void in
+            view.transform = (anim.data["isBy"] as! Bool) ? CGAffineTransformRotate(view.transform, a) : CGAffineTransformMakeRotation(a)
+            }) { (v) -> Void in
+                if v{ comp?() }
+        }
+    }
+    
+    class private func _handleSequence(view : UIView, _ anim : UIAnimation, _ comp: (()->Void)?){
+        _run(view, anim.data["animations"] as! [UIAnimation], 0, comp)
+    }
+    
+    class private func _handleGroup(view : UIView, _ anim : UIAnimation, _ comp: (()->Void)?){
+        let anims = anim.data["animations"] as! [UIAnimation]
+        var n_comp = 0
+        for a in anims{
+            a.handler(view,a,{ () -> Void in
+                if(++n_comp == anims.count){
+                    comp?()
+                }})}
+    }
+    
+    class private func _handleRepeat(view : UIView, _ anim : UIAnimation, _ comp: (()->Void)?){
+        _repetition(view, anim.data["animation"] as! UIAnimation, anim.data["repTime"] as! Int, comp)
+    }
+    
+    class private func _handleBlock(view : UIView, _ anim : UIAnimation, _ comp: (()->Void)?){
+        (anim.data["block"] as! BlockWrapper).block();
+        comp?();
+    }
+    
+    class private func _handleWait(view : UIView, _ anim : UIAnimation, _ comp: (()->Void)?){
+        UIView.animateWithDuration(anim.time, animations: {}) { (v) -> Void in
+            if v{ comp?() }
+        }
+    }
+    
+    class private func _run(view : UIView, _ anim : [UIAnimation], var _ i : Int, _ completion : (()->Void)?){
+        let curr = anim[i]
+        curr.handler(view,curr,{ () -> Void in
+            if (++i)<anim.count{
+                _run(view, anim, i, completion)
+            }
+            else{ completion?() }
+        })
+    }
+    
+    class private func _repetition(view : UIView, _ anim : UIAnimation, var _ rep : Int, _ completion : (()->Void)?){
+        anim.handler(view,anim,{ () -> Void in
+            if (rep <= -1 || --rep>0){
+                _repetition(view, anim, rep, completion)
+            }
+            else{ completion?() }
+        })
+    }
+}
---
+++ UIAnimation/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  UIAnimation
+//
+//  Created by Pietro Ribeiro Pepe on 3/31/16.
+//  Copyright © 2016 Pietro Ribeiro Pepe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let v = UIView()
+        v.backgroundColor = UIColor.redColor()
+        v.center = view.center
+        v.frame.size = CGSizeMake(view.frame.width/3, view.frame.width/3)
+        view.addSubview(v)
+        
+        let point1 = CGPointMake(v.frame.width*1.5/2, view.frame.height/2)
+        let point2 = CGPointMake(view.frame.width-point1.x, point1.y)
+        
+        
+        let anim = UIAnimation.moveTo(point1, duration: 1) // This action moves to point1
+        let anim2 = UIAnimation.moveTo(point2, duration: 1) // This action moves to point2
+        
+        let rot1 = UIAnimation.rotateBy(90, duration: 0.6) // Rotates 90 degrees clockwise
+        let rot2 = UIAnimation.rotateBy(-90, duration: 0.6) // Rotates 90 degrees anti-clockwise
+        
+        
+        let seq = UIAnimation.sequence([anim,rot1,anim2,rot2]) // Executes, in the order, the actions given
+        let rep = UIAnimation.repeatAnimationForever(seq) // Makes the sequence above repeats forever
+        
+        v.runAnimation(rep) // Executes the animation
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
